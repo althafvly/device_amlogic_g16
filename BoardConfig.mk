29917dfd6ada4bc8c16d8cf3cbb1a19aeb4f128d
Nicholas Lim <nicknitewolf@lineageos.org>
2022-04-21 18:11:20 +0000

    x96w: Use beast defconfig since we use a s905w

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,8 +11,9 @@ DEVICE_PATH := device/amlogic/x96w
 ## Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := x96w
 
-## DTB
+## Kernel
 TARGET_DTB_NAME := gxl_p281_1g gxl_p281_2g
+TARGET_KERNEL_CONFIG := gxl_defconfig
 
 # SEPolicy (ew)
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
