41c6f51211c562835f460f90cc706892fc04c269
Nicholas Lim <nicknitewolf@lineageos.org>
2022-04-23 17:31:38 +0000

    q6x: Use utgard drivers for mali450
     * Our s905w uses a mali450

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,12 @@ DEVICE_PATH := device/amlogic/q6x
 ## Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := q6x
 
+## Display
+GPU_TYPE := mali450
+GPU_ARCH := utgard
+MALI_GPU_VARIANT := utgard
+MALI_DRV_VERSION := r10p1
+
 ## Kernel
 TARGET_DTB_NAME := gxl_p281_1g gxl_p281_2g
 TARGET_KERNEL_CONFIG := gxl_defconfig
